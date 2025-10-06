12c7532539d6afb8c9028fa55fc79218b9b74fda
Cynthia Wang <hyacccint@gmail.com>
2025-10-06 13:22:42 -0400

    fix: firebase config placeholders

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebas
 import { getAI, getGenerativeModel, GoogleAIBackend, InferenceMode, Schema } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-ai.js";
 
 const firebaseConfig = {
-
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
